d7dbb956fcbe10bc777af41f5a16358a8dea8847
farikkm <farikk.v.2@gmail.com>
2025-04-08 21:28:41 +0500

    Main Page Layout is ready

---
+++ src/components/home/FilterPanel.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowUpNarrowWide } from "lucide-react";
+
+const filterOptions = [
+  "Chicken",
+  "Meat",
+  "Fish",
+  "Vegetarian",
+  "Vegan",
+  "Chicken",
+  "Meat",
+  "Fish",
+  "Vegetarian",
+  "Vegan",
+];
+
+export default function FilterPanel() {
+  const [activeFilters, setActiveFilters] = useState<string[]>([]);
+
+  const toggleFilter = (filter: string) => {
+    setActiveFilters((prev) =>
+      prev.includes(filter)
+        ? prev.filter((f) => f !== filter)
+        : [...prev, filter]
+    );
+  };
+
+  return (
+    <div className="my-10 flex justify-between items-center flex-wrap gap-2 p-2 bg-gray-100 rounded-2xl w-full">
+      {filterOptions.map((option) => (
+        <Button
+          key={option}
+          variant={activeFilters.includes(option) ? "default" : "outline"}
+          onClick={() => toggleFilter(option)}
+          className="rounded-full py-6 px-5 bg-transparent hover:bg-gray-300 outline-none border-none ring-0"
+        >
+          {option}
+        </Button>
+      ))}
+      <Button
+        className="text-black rounded-full py-6 px-5 bg-transparent hover:bg-gray-300 outline-none border-none ring-0"
+      >
+        <ArrowUpNarrowWide />
+        Sorting
+      </Button>
+    </div>
+  );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,13 @@
 import Header from "@/components/header/Header";
 import Slider from "@/components/slider/Slider";
 
+import "keen-slider/keen-slider.min.css";
+import foodImg from "@/assets/images/food.jpg";
+import { CarFront, Star } from "lucide-react";
+import FilterPanel from "@/components/home/FilterPanel";
+
+const IMG_HEIGHT = 200;
+
 const Home = () => {
   return (
     <>
@@ -9,18 +16,52 @@ const Home = () => {
         <div className="container mx-auto px-5 space-y-4">
           <h2 className="text-3xl font-bold">Вы заказывали: </h2>
           <Slider />
-        </div>
-        <div className="container mx-auto px-5 space-y-4">
-          <h2 className="text-3xl font-bold">Вы заказывали: </h2>
+          <FilterPanel />
+          <h2 className="text-3xl font-bold">Предложения: </h2>
           <Slider />
-        </div>
-        <div className="container mx-auto px-5 space-y-4">
-          <h2 className="text-3xl font-bold">Вы заказывали: </h2>
-          <Slider />
-        </div>
-        <div className="container mx-auto px-5 space-y-4">
-          <h2 className="text-3xl font-bold">Вы заказывали: </h2>
-          <Slider />
+          <div className="mt-10 space-y-4">
+            <h2 className="text-3xl font-bold">Рестораны</h2>
+            <div className="flex gap-4 flex-wrap *:grow">
+              {[...Array(16)].map((_, index) => (
+                <div key={index} className="w-[23.5%] h-[23.5%]">
+                  <div className="flex flex-col gap-2">
+                    <div
+                      style={{ height: IMG_HEIGHT }}
+                      className="relative flex items-center justify-center overflow-hidden rounded-3xl group"
+                    >
+                      <img
+                        src={foodImg}
+                        className="absolute object-cover h-full w-full"
+                        alt="food-jpg"
+                      />
+                      <div className="absolute inset-0 bg-black/0 transition-all duration-300 group-hover:bg-black/6" />
+                    </div>
+                    <div className="px-2 space-y-1.5">
+                      <div className="flex items-center justify-between">
+                        <h5 className="font-medium">KFC</h5>
+                        <div className="flex items-center gap-2">
+                          <Star size={16} fill="black" />
+                          <span className="text-sm">4.7</span>
+                        </div>
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <CarFront size={20} />
+                        <span className="text-sm">15 - 25 min</span>
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <button className="cursor-pointer px-2 py-1 bg-gray-200 rounded-3xl text-sm font-medium">
+                          Free Delivery
+                        </button>
+                        <button className="cursor-pointer px-2 py-1 bg-gray-200 rounded-3xl text-sm font-medium">
+                          You ordered
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </main>
     </>
